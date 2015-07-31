9d2fe1894da06c05a3ae9b6622048c637805d9a7
srequiz <srequiz@gmail.com>
2015-07-31 14:15:29 +0100

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller'); //añadimos el controlador de preguntas
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
